89d3df27676427e25dfef6077b81722032498e9a
liaosw97 <1414229513@qq.com>
2021-01-07 18:56:36 +0800

    Update StaticTest.java

--- StaticTest.java
+++ StaticTest.java
@@ -2,4 +2,13 @@ class StaticTest {
     static i = 47;
 }
 
-class 
+public static void main( String [] args) {
+    StaticTest st1 = new StaticTest();
+    StaticTest st2 = new StaticTest();
+    
+    System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+    
+    StaticTest.i++;    
+    System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+}
+ 
