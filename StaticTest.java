f096db360d580ec069e1ccabc1303aa1ab368b69
liaosw97 <1414229513@qq.com>
2021-01-19 11:18:50 +0800

    Update StaticTest.java

--- StaticTest.java
+++ StaticTest.java
@@ -1,27 +1,31 @@
-class StaticTest {
-    static int i = 47;
-}
-
 class Incrementable {
     static void increment() {
         StaticTest.i++;
     }
 }
 
-public static void main( String [] args) {
-    StaticTest st1 = new StaticTest();
-    StaticTest st2 = new StaticTest();
+class StaticTest {
+    static int i = 47;
     
-    System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+    public static void main( String [] args) {
+        StaticTest st1 = new StaticTest();
+        StaticTest st2 = new StaticTest();
     
-    StaticTest.i++;    
-    System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+        System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
     
-    Incrementable sf = new Incrementanle();
-    sf.increment();
-    System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+        StaticTest.i++;    
+        System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
     
-    Incrementable.increment();
-    System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+        Incrementable sf = new Incrementanle();
+        sf.increment();
+        System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+    
+        Incrementable.increment();
+        System.out.println("st1.i =" + st1.i +"\n" + "st2.i =" + st2.i );
+    }
 }
+
+
+
+
  
