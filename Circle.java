2212c274684496aa808e81007d1885999a00aee6
liaosw97 <1414229513@qq.com>
2021-01-19 19:04:56 +0800

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -28,4 +28,16 @@ public class Square extends Shape {
         System.out.println("Square.erase()");
     }
 }
+
+public class Triangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Triangle.draw()");
+    }
+    
+    @Override
+    public void erase() {
+        System.out.println("Trangle.erase()");
+    }
+}
         
