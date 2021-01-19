914b672f251efa4bab89e3e36eba2d06e940c5c4
liaosw97 <1414229513@qq.com>
2021-01-19 19:23:25 +0800

    Update RandomShapes.java

--- RandomShapes.java
+++ RandomShapes.java
@@ -22,3 +22,13 @@ public class RandomShapes {
         return shapes;
     }
 }
+
+public static Shapes {
+    public static void main(String[] args) {
+        RandomShapes gen = new RandomShapes();
+        
+        for (Shape shape: gen.array(9)) {
+            shape.draw();
+        }
+    }
+}
